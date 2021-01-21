9d8bef3192b991d859f3ce288b58f4863951f47e
grnch <grnch.net@gmail.com>
2021-01-21 19:31:10 +0200

    refactory debug

---
+++ capacity_quadtree.js
@@ -0,0 +1,120 @@
+class Boundary {
+
+  constructor(x, y, w, h) {
+    this.x1 = x;
+    this.y1 = y;
+    this.x2 = w;
+    this.y2 = h;
+  }
+
+}
+
+class QuadTree {
+
+  constructor(boundary, capacity = 120) {
+    this.boundary = boundary;
+    this.capacity = capacity;
+
+    this.nw = null;
+    this.ne = null;
+    this.sw = null;
+    this.se = null;
+
+    this.points = [];
+  }
+
+  insert(point) {
+    var b = this.boundary;
+
+    if (b.x1 > point.x ||
+      b.x2 < point.x ||
+      b.y1 > point.y ||
+      b.y2 < point.y) {
+      return false;
+    }
+
+    if (this.points.length < this.capacity && this.nw == null) {
+      this.points.push(point);
+      return true;
+    } else {
+      if (this.nw == null) {
+        this.subdivide();
+      }
+
+      return this.nw.insert(point) ||
+        this.ne.insert(point) ||
+        this.sw.insert(point) ||
+        this.se.insert(point);
+    }
+
+    return false;
+  }
+
+  subdivide() {
+    var b = this.boundary;
+    var x = b.x1;
+    var y = b.y1;
+    var w = b.x2;
+    var h = b.y2;
+    var x_mid = (x + w) / 2;
+    var y_mid = (y + h) / 2;
+
+    var nw_boundary = new Boundary(x, y, x_mid, y_mid);
+    var ne_boundary = new Boundary(x_mid, y, w, y_mid);
+    var sw_boundary = new Boundary(x, y_mid, x_mid, h);
+    var se_boundary = new Boundary(x_mid, y_mid, w, h);
+
+    this.nw = new QuadTree(nw_boundary, this.capacity);
+    this.ne = new QuadTree(ne_boundary, this.capacity);
+    this.sw = new QuadTree(sw_boundary, this.capacity);
+    this.se = new QuadTree(se_boundary, this.capacity);
+
+    for (var i = 0; i < this.points.length; i++) {
+      this.nw.insert(this.points[i]) ||
+        this.ne.insert(this.points[i]) ||
+        this.sw.insert(this.points[i]) ||
+        this.se.insert(this.points[i]);
+    }
+
+    this.points = [];
+  }
+
+  findByRadius(point, radius) {
+    if (this.nw != null) {
+      return this.nw.findByRadius(point, radius).concat(
+        this.ne.findByRadius(point, radius),
+        this.sw.findByRadius(point, radius),
+        this.se.findByRadius(point, radius)
+      );
+    } else {
+      if (this._checkIntersectionWithCircle(point, radius, this.boundary)) {
+        var return_array = [];
+        for (var i = 0; i < this.points.length; i++) {
+          if (Math.sqrt(Math.pow(this.points[i].x - point.x, 2) + Math.pow(this.points[i].y - point.y, 2)) <= radius) {
+            return_array.push(this.points[i]);
+          }
+        }
+        return return_array;
+      } else {
+        return [];
+      }
+    }
+  }
+
+  _checkIntersectionWithCircle(point, radius, boundary) {
+    if (boundary.x2 < point.x - radius ||
+      boundary.x1 > point.x + radius ||
+      boundary.y1 > point.y + radius ||
+      boundary.y2 < point.y - radius
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+}
+
+export {
+  QuadTree
+};
--- debug.js
+++ debug.js
@@ -1,10 +1,6 @@
-var window_size_x = 100;
-var window_size_y = 100;
+import { QuadTree } from './capacity_quadtree.js';
 
-var points_count_limit = 20000;
 var found_points = [];
-var points = [];
-var qt_capacity = 120; // 40 for 10000, 120 for 20000, 
 
 class Point {
 
@@ -17,8 +13,8 @@ class Point {
 
     let _points_full = [];
     for (let i = 0; i < pointsSelected.length; i++) {
-      if (points[pointsSelected[i]].x != this.x && points[pointsSelected[i]].y != this.y) {
-        _points_full.push([points[pointsSelected[i]].x, points[pointsSelected[i]].y]);
+      if (pointsSelected[i].x != this.x && pointsSelected[i].y != this.y) {
+        _points_full.push([pointsSelected[i].x, pointsSelected[i].y]);
       }
     }
 
@@ -30,160 +26,46 @@ class Point {
     }
 
   }
+
 }
 
 class Boundary {
 
   constructor(x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-  }
-}
-
-class QuadTree {
-  constructor(boundary, capacity) {
-    this.boundary = boundary;
-    this.capacity = capacity;
-
-    this.nw = null;
-    this.ne = null;
-    this.sw = null;
-    this.se = null;
-
-    this.points = [];
-  }
-
-  insertPoint(point_index) {
-    var b = this.boundary;
-
-    if (b.x > points[point_index].x ||
-      b.w < points[point_index].x ||
-      b.y > points[point_index].y ||
-      b.h < points[point_index].y) {
-      return false;
-    }
-
-    if (this.points.length < this.capacity && this.nw == null) {
-      this.points.push(point_index);
-      points[point_index].modified = false;
-      return true;
-    } else {
-
-      if (this.nw == null) {
-        this.subdivide();
-      }
-
-      return this.nw.insertPoint(point_index) ||
-        this.ne.insertPoint(point_index) ||
-        this.sw.insertPoint(point_index) ||
-        this.se.insertPoint(point_index);
-    }
-
-    return false;
-  }
-
-  subdivide() {
-    var b = this.boundary;
-    var x = b.x;
-    var y = b.y;
-    var w = b.w;
-    var h = b.h;
-    var x_mid = (x + w) / 2;
-    var y_mid = (y + h) / 2;
-
-    var nw_boundary = new Boundary(x, y, x_mid, y_mid);
-    var ne_boundary = new Boundary(x_mid, y, w, y_mid);
-    var sw_boundary = new Boundary(x, y_mid, x_mid, h);
-    var se_boundary = new Boundary(x_mid, y_mid, w, h);
-
-    this.nw = new QuadTree(nw_boundary, this.capacity);
-    this.ne = new QuadTree(ne_boundary, this.capacity);
-    this.sw = new QuadTree(sw_boundary, this.capacity);
-    this.se = new QuadTree(se_boundary, this.capacity);
-
-    for (var i = 0; i < this.points.length; i++) {
-      this.nw.insertPoint(this.points[i]) ||
-        this.ne.insertPoint(this.points[i]) ||
-        this.sw.insertPoint(this.points[i]) ||
-        this.se.insertPoint(this.points[i]);
-    }
-
-    this.points = [];
-  }
-
-  selectPointsForCircleArea(x, y, radius) {
-
-    if (this.nw != null) {
-      return this.nw.selectPointsForCircleArea(x, y, radius).concat(
-        this.ne.selectPointsForCircleArea(x, y, radius),
-        this.sw.selectPointsForCircleArea(x, y, radius),
-        this.se.selectPointsForCircleArea(x, y, radius)
-      );
-    } else {
-      if (this.checkIntersectionWithCircle(x, y, radius, this.boundary)) {
-        var return_array = [];
-        for (var i = 0; i < this.points.length; i++) {
-          if (Math.sqrt(Math.pow(points[this.points[i]].x - x, 2) + Math.pow(points[this.points[i]].y - y, 2)) <= radius) {
-            return_array.push(this.points[i]);
-          }
-        }
-        return return_array;
-      } else {
-        return [];
-      }
-    }
-  }
-
-  checkIntersectionWithCircle(x, y, radius, boundary) {
-    if (boundary.w < x - radius || boundary.x > x + radius || boundary.y > y + radius || boundary.h < y - radius) {
-      return false;
-    }
-
-    return true;
+    this.x1 = x;
+    this.y1 = y;
+    this.x2 = w;
+    this.y2 = h;
   }
 
 }
 
-var boundary;
-var selected_boundary;
+const window_size_x = 100;
+const window_size_y = 100;
 
-console.log(`%c ${new Date().toJSON()} -> --start`, 'color: #bada55');
+const points_count_limit = 20000;
 
-boundary = new Boundary(0, 0, window_size_x, window_size_y);
+const boundary = new Boundary(0, 0, window_size_x, window_size_y);
 
-for (var i = 0; i < points_count_limit; i++) {
-  points.push(new Point(Math.floor(Math.random() * window_size_x), Math.floor(Math.random() * window_size_y)));
+const tree = new QuadTree(boundary);
+const points = [];
+
+for (let i = 0; i < points_count_limit; i++) {
+  const point = new Point(
+    Math.floor(Math.random() * window_size_x),
+    Math.floor(Math.random() * window_size_y)
+  );
+  points.push(point);
+  tree.insert(point);
 }
 
+for (let i = 0; i < points_count_limit; i++) {
+  const pointsSelected = tree.findByRadius(points[i], 5);
 
-var qt = new QuadTree(boundary, qt_capacity);
-
-for (var i = 0; i < points_count_limit; i++) {
-  qt.insertPoint(i);
-}
-
-showAndMovePoints(qt);
-
-console.log(`%c ${new Date().toJSON()} -> --end`, 'color: #bada55');
-
-
-function showAndMovePoints(qt) {
-
-  var _old_points = points;
-
-  for (var i = 0; i < points_count_limit; i++) {
-
-    var _point = points[i];
-    var pointsSelected = qt.selectPointsForCircleArea(_point.x, _point.y, 5);
-
-    var _intersected_points_count = pointsSelected.length;
-    if (_intersected_points_count > 1) {
-      _point.show(pointsSelected);
-    }
+  if (pointsSelected.length > 1) {
+    points[i].show(pointsSelected);
   }
-
-  console.log(JSON.stringify(found_points));
-
 }
+
+console.log(performance.now(), '-> --end');
+console.log(found_points);
